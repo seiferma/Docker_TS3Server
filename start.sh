a638c1981f7e49ac428cef31c840e9b099e17bd0
Stephan Seifermann <seiferma@users.noreply.github.com>
2018-02-16 21:59:49 +0100

    Accept license agreement.

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 [ ! -f $DATA_VOLUME/ts3server.sqlitedb ] && touch $DATA_VOLUME/ts3server.sqlitedb
 ln -s $DATA_VOLUME/ts3server.sqlitedb ts3server.sqlitedb
 export LANG=en_US.UTF-8
+export TS3SERVER_LICENSE=accept
 ./ts3server_minimal_runscript.sh
